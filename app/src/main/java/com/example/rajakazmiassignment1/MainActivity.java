b0092015048a379806d3e67915958509c95b5b79
kasim <kasimkazmi27@gmail.com>
2024-09-28 01:38:41 -0400

    Logic added for Conversion

--- app/src/main/java/com/example/rajakazmiassignment1/MainActivity.java
+++ app/src/main/java/com/example/rajakazmiassignment1/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.rajakazmiassignment1;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,8 +29,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // To get current time
         TextView currentTime =findViewById(R.id.time);
         currentTime.setText(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
+
+        // Variable assign
+        EditText inputTemperature =findViewById(R.id.input_Temprature);
+        Button celsiusButton =findViewById(R.id.celsius);
+        Button fahrenheitButton =findViewById(R.id.fahrenheit);
+        TextView resultTextView = findViewById(R.id.result);
+
+        // OnClickListener for Celsius
+
+        celsiusButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onClick(View v) {
+                String input = inputTemperature.getText().toString();
+                if (input.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Please enter a value", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                try {
+                    double temperature =Double.parseDouble(input);
+                    double celsius = (temperature -32)* 5/9;  //logic to calculate celsius
+                    resultTextView.setText(String.format("%.2f°C",celsius));
+                }
+                catch ( NumberFormatException error)
+                {
+                    Toast.makeText(MainActivity.this, "Invalid input", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        // OnClickListener for Fahrenheit
+        fahrenheitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = inputTemperature.getText().toString();
+                if (input.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter a temperature value", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                try {
+                    double temperature = Double.parseDouble(input);
+                    double fahrenheit = temperature * 9 / 5 + 32;
+                    resultTextView.setText(String.format("%.2f°F", fahrenheit)); //logic to calculate Fahrenheit
+                } catch (NumberFormatException e) {
+                    Toast.makeText(MainActivity.this, "Invalid input", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
