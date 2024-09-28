922451652f34b6536833f77c494732ebc1ce51bf
kasim <kasimkazmi27@gmail.com>
2024-09-28 00:30:36 -0400

    UI Created

---
+++ app/src/main/java/com/example/rajakazmiassignment1/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.rajakazmiassignment1;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        TextView currentTime =findViewById(R.id.time);
+        currentTime.setText(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        }
+
+
+        );
+
+
+
+    }
+}
